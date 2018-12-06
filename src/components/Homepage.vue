507d516212a61cda45cc7b18e3149f1b1b6c8a52
Lloyd Headbush <ocanica@gmail.com>
2018-12-06 13:17:21 +0000

    Initial commit

---
+++ src/components/Homepage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Year</th>
+                    <th>Genres</th>
+                    <th>Rating</th>
+                    <th>Director</th>
+                    <th>Actors</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+        <tbody>
+            <template v-for="movie in movies">
+                <tr v-bind:key="movie.id">
+                    <td>{{ movie.id }}</td>
+                    <td><a v-bind:href="movie.imdb_link" target="_blank">{{ movie.movie_title }}</a></td>
+                    <td>{{ movie.title_year }}</td>
+                    <td>{{ movie.genres }}</td>
+                    <td>{{ movie.content_rating }}</td>
+                    <td>{{ movie.director }}</td>
+                    <td>{{ movie.actor_1 }}</td>
+                    <td>{{ movie.imdb_score }}</td>
+                </tr>
+            </template>
+        </tbody>
+    </table>
+    <a class="button is-info">Add Movie</a>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            movies: {}
+        }
+    },
+    async created () {
+        const response = await axios.get('http://localhost:8000/movies')
+        this.movies = response.data
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter); 
+
+import Homepage from './components/Homepage.vue';
+const routes = [
+  { path: '/', component: Homepage }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
-  render: h => h(App)
+  router,
+  render: h => h(Homepage)
 }).$mount('#app')
